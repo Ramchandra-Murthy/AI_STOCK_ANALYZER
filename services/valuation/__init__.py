2bac6da9a1dc5d67604efb949c998270bd7dbd59
Ramchandra Murthy <profrsmurthy@gmail.com>
2026-08-06 17:44:45 +0530

    refactor(valuation): register SOTPValuationEngine in get_valuation_dispatcher public boundary

--- services/valuation/__init__.py
+++ services/valuation/__init__.py
@@ -1,12 +1,14 @@
 ﻿from services.valuation.dcf_engine import DCFEngine
 from services.valuation.dispatcher import ValuationDispatcher
 from services.valuation.nav_engine import NAVEngine
+from services.valuation.sotp_engine import SOTPValuationEngine
 
 
 def get_valuation_dispatcher() -> ValuationDispatcher:
     dispatcher = ValuationDispatcher()
     dispatcher.register(DCFEngine())
     dispatcher.register(NAVEngine())
+    dispatcher.register(SOTPValuationEngine())
     return dispatcher
 
 
@@ -14,5 +16,6 @@ __all__ = [
     "ValuationDispatcher",
     "DCFEngine",
     "NAVEngine",
+    "SOTPValuationEngine",
     "get_valuation_dispatcher",
 ]
