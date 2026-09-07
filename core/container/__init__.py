d9843aabb52fe2f9d9ee9559acec1e15260d1150
Ramchandra Murthy <58673036+Ramchandra-Murthy@users.noreply.github.com>
2026-09-07 21:39:16 +0530

    repair: expose compatible EROS container API

--- core/container/__init__.py
+++ core/container/__init__.py
@@ -1,4 +1,20 @@
-﻿from core.container.container import container, ServiceContainer
+"""Compatibility exports for the EROS application container."""
+
+from core.container.container import ServiceContainer, container
+from core.container.exceptions import (
+    ContainerError,
+    DuplicateServiceError,
+    ServiceNotFoundError,
+)
 from core.container.registry import ServiceKey
 from core.container.bootstrap import bootstrap_container
-from core.container.exceptions import ContainerError, ServiceNotFoundError, DuplicateServiceError
+
+__all__ = [
+    "ServiceContainer",
+    "container",
+    "ServiceKey",
+    "bootstrap_container",
+    "ContainerError",
+    "ServiceNotFoundError",
+    "DuplicateServiceError",
+]
