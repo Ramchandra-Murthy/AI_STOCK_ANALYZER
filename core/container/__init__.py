cd58aef85c6fd7fcc7837fa412c582b1c7cf7e72
Ramchandra Murthy <58673036+Ramchandra-Murthy@users.noreply.github.com>
2026-09-10 23:38:07 +0530

    repair: resolve container init merge conflict and BOM

--- core/container/__init__.py
+++ core/container/__init__.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿from __future__ import annotations
+from __future__ import annotations
 
 from core.container.container import Container
 from core.container.registry import ServiceRegistry
@@ -13,25 +12,4 @@ __all__ = [
     "SingletonProvider",
     "TransientProvider",
     "ContainerBootstrap",
-=======
-"""Compatibility exports for the EROS application container."""
-
-from core.container.container import ServiceContainer, container
-from core.container.exceptions import (
-    ContainerError,
-    DuplicateServiceError,
-    ServiceNotFoundError,
-)
-from core.container.registry import ServiceKey
-from core.container.bootstrap import bootstrap_container
-
-__all__ = [
-    "ServiceContainer",
-    "container",
-    "ServiceKey",
-    "bootstrap_container",
-    "ContainerError",
-    "ServiceNotFoundError",
-    "DuplicateServiceError",
->>>>>>> afd58f7f54a3fb77a08bb3a8a1c4b4e5e498fe47
 ]
