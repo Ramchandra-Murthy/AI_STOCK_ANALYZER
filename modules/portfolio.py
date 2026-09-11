31b7b93121cc0c243366b1e839525724a0f5e648
Ramchandra Murthy <58673036+Ramchandra-Murthy@users.noreply.github.com>
2026-09-11 11:32:06 +0530

    EROS: preserve portfolio module exports

--- modules/portfolio.py
+++ modules/portfolio.py
@@ -1,5 +1,3 @@
-from portfolio.portfolio import (
-    add_stock,
-    create_portfolio_table,
-    delete_stock,
-)
+from portfolio.portfolio import add_stock, create_portfolio_table, delete_stock
+
+__all__ = ["add_stock", "create_portfolio_table", "delete_stock"]
