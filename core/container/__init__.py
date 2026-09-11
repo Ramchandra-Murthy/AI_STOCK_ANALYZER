191406c34211ded5c1caac397a05d284c922d887
Ramchandra Murthy <58673036+Ramchandra-Murthy@users.noreply.github.com>
2026-09-11 11:19:26 +0530

    EROS: normalize container imports

--- core/container/__init__.py
+++ core/container/__init__.py
@@ -1,4 +1,4 @@
-﻿from core.container.container import container, ServiceContainer
-from core.container.registry import ServiceKey
 from core.container.bootstrap import bootstrap_container
-from core.container.exceptions import ContainerError, ServiceNotFoundError, DuplicateServiceError
+from core.container.container import ServiceContainer, container
+from core.container.exceptions import ContainerError, DuplicateServiceError, ServiceNotFoundError
+from core.container.registry import ServiceKey
