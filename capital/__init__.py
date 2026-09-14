f9c1df38907227f4622e574f47276fc80e9bc350
Ramchandra Murthy <58673036+Ramchandra-Murthy@users.noreply.github.com>
2026-09-14 17:47:12 +0530

    Fix capital package eager import compatibility

--- capital/__init__.py
+++ capital/__init__.py
@@ -1,5 +1,10 @@
 """Capital valuation compatibility package."""
 
-from .wacc_engine import CAPMOutput, CapitalCostEngine
-
 __all__ = ["CAPMOutput", "CapitalCostEngine"]
+
+
+def __getattr__(name: str):
+    if name in __all__:
+        from .wacc_engine import CAPMOutput, CapitalCostEngine
+        return {"CAPMOutput": CAPMOutput, "CapitalCostEngine": CapitalCostEngine}[name]
+    raise AttributeError(name)
