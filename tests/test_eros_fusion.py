67b477948205d2abd24a56e135ed063f209cdf57
Ramchandra Murthy <58673036+Ramchandra-Murthy@users.noreply.github.com>
2026-09-18 19:50:18 +0530

    Test EROS price-jump persistence weighting

--- tests/test_eros_fusion.py
+++ tests/test_eros_fusion.py
@@ -32,3 +32,37 @@ def test_fusion_combines_available_components():
 
 def test_empty_confluence_returns_empty():
     assert compute_eros_fusion(pd.DataFrame()).empty
+
+
+
+def test_price_jump_persistence_replaces_binary_presence_component():
+    confluence = pd.DataFrame(
+        {
+            "Symbol": ["AAA", "BBB"],
+            "Exchange": ["NSE", "NSE"],
+            "Confluence Score": [80.0, 80.0],
+        }
+    )
+    jumps = pd.DataFrame({"Symbol": ["AAA", "BBB"], "Exchange": ["NSE", "NSE"]})
+    jump_history = pd.DataFrame(
+        {
+            "Timestamp": pd.to_datetime(
+                ["2026-09-18 10:00", "2026-09-18 10:05", "2026-09-18 10:10"]
+            ),
+            "Symbol": ["AAA", "AAA", "AAA"],
+            "Exchange": ["NSE", "NSE", "NSE"],
+        }
+    )
+
+    result = compute_eros_fusion(
+        confluence,
+        None,
+        jumps,
+        {"score": 50.0},
+        jump_history,
+    )
+
+    aaa = result.loc[result["Symbol"] == "AAA"].iloc[0]
+    bbb = result.loc[result["Symbol"] == "BBB"].iloc[0]
+    assert aaa["Price Jump Component"] == 60.0
+    assert bbb["Price Jump Component"] == 0.0
