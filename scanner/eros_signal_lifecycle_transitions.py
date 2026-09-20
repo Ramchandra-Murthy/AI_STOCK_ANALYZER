3a7e2f21959610832b96a8c0c8ad320a124aaaab
Ramchandra Murthy <58673036+Ramchandra-Murthy@users.noreply.github.com>
2026-09-20 15:49:57 +0530

    Satisfy Ruff explicit zip strict setting

--- scanner/eros_signal_lifecycle_transitions.py
+++ scanner/eros_signal_lifecycle_transitions.py
@@ -50,7 +50,7 @@ def analyze_eros_signal_lifecycle_transitions(
         )
         transitions = sum(
             current != prior
-            for prior, current in zip(lifecycle_values, lifecycle_values[1:])
+            for prior, current in zip(lifecycle_values, lifecycle_values[1:], strict=False)
         )
 
         rows.append(
